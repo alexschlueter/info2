2413bfb3611839269cb3ee5c8e590e8da7acfec4
Alexander Schlüter <alx.schlueter@gmail.com>
2016-05-04 16:32:55 +0200

    b3a8 pdf, automatisches latex

--- b3/a8/src/Main.java
+++ b3/a8/src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
     public static final <T> void printMatrix(T[][] a) {
@@ -8,24 +8,65 @@ public class Main {
         }
     }
 
+    public static final <T> void latexMatrix(Integer k, T[][] a, Consumer<T> printEl, String matr) {
+        if (k % 2 == 0) System.out.print("& ");
+        System.out.println(matr + "^{" + k.toString() + "} &=");
+        System.out.println("\\begin{bmatrix}");
+        for (int i = 0; i < a.length; ++i) {
+            for (int j = 0; j < a[i].length; ++j) {
+                printEl.accept(a[i][j]);
+                if (j != a[i].length - 1) {
+                    System.out.print(" &");
+                }
+                System.out.print(" ");
+            }
+            if (i != a.length - 1) {
+                System.out.print(" \\\\");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\\end{bmatrix}");
+        if (k % 2 == 0) System.out.print(" \\\\");
+        System.out.print("\n");
+    }
+
+    public static final void printBool(Boolean b) {
+        System.out.print("\\text{" + (b ? "T" : "F") + "}");
+    }
+
+    public static final void printDouble(Double d) {
+        if (d.isInfinite()) {
+            System.out.print("\\infty");
+        } else {
+            System.out.print((int)d.doubleValue());
+        }
+    }
+
     public static final Boolean[][] warshall(Boolean[][] a) {
+        for (int i = 0; i < a.length; ++i) {
+            a[i][i] = true;
+        }
+        latexMatrix(-1, a, (b) -> printBool(b), "B");
         for (int k = 0; k < a.length; ++k) {
             for (int i = 0; i < a.length; ++i) {
                 for (int j = 0; j < a.length; ++j) {
                     a[i][j] = a[i][j] || (a[i][k] && a[k][j]);
                 }
             }
+            latexMatrix(k, a, (b) -> printBool(b), "B");
         }
         return a;
     }
 
     public static final Double[][] floyd(Double[][] a) {
+        latexMatrix(-1, a, (d) -> printDouble(d), "D");
         for (int k = 0; k < a.length; ++k) {
             for (int i = 0; i < a.length; ++i) {
                 for (int j = 0; j < a.length; ++j) {
                     a[i][j] = Math.min(a[i][j], a[i][k] + a[k][j]);
                 }
             }
+            latexMatrix(k, a, (d) -> printDouble(d), "D");
         }
         return a;
     }
